6b8ebce6a51320376ec377ae019041a99a2ac0e9
ibrahim <ibrahimfe2o0o@gmail.com>
2025-07-21 00:53:13 +0300

    handled view

--- src/features/dashboard/users-management/view/view.component.ts
+++ src/features/dashboard/users-management/view/view.component.ts
@@ -10,40 +10,73 @@ import { UsersManagmentsService } from '../../../../core/services/users-managmen
   styleUrl: './view.component.scss'
 })
 export class ViewComponent {
-  constructor(private _UsersManagmentsService:UsersManagmentsService,private _ActivatedRoute:ActivatedRoute,private sharedService: SharedService) {
+  constructor(private _UsersManagmentsService: UsersManagmentsService, private _ActivatedRoute: ActivatedRoute, private sharedService: SharedService) {
     this.sharedService.breadCrumbTitle.next('View User');
     this._ActivatedRoute.paramMap.subscribe((res) => {
       forkJoin([
-              this._UsersManagmentsService.getPermssionsTypes(),
-          this._UsersManagmentsService.getDevicesTypes(),
-          this._UsersManagmentsService.getUserById(res.get('id')),
+        this._UsersManagmentsService.getPermssionsTypes(),
+        this._UsersManagmentsService.getPermsionsDevices(),
+        this._UsersManagmentsService.getUserById(res.get('id')),
       ]).subscribe((res: any[]) => {
-          const permssions = res[0]?.data;
-        const devicesTypes = res[1]?.data;
-        this.userDataShow = res[2]?.data;
-                 const userDevicePermissions = this.userDataShow?.devicePermissions || [];
-          this.devicesTypes.set(
-            devicesTypes.map((device: any) => {
-              // Find user permissions for this device type
-              const userDevice = userDevicePermissions.find((d: any) => d.deviceTypeId === device.id);
-              return {
-                ...device,
-                permssions: permssions.map((perm: any) => ({
-                  ...perm,
-                  isSelected: userDevice
-                    ? userDevice.permissions.some((p: any) => p.id === perm.id)
-                    : false
-                }))
-              };
-            })
-        );
+        const user = res[2]?.data;
+        this.userDataShow = user
+        const deviceTypes = res[1]?.data?.deviceTypes || [];
+        const permissions = res[1]?.data?.permissions || [];
+        const userDevices = user?.devices || [];
+        const structured = deviceTypes.map((type: any) => {
+          const devices = (type.devices || []).map((dev: any) => {
+            const userDevice = userDevices.find((ud: any) => ud.deviceId === dev.id);
 
-        this.loading_data.set(false);
+            const devPerms = permissions.map((perm: any) => ({
+              ...perm,
+              isSelected: !!userDevice?.permissions.some((p: any) => p.id === perm.id)
+            }));
+
+            const is_selected_all = devPerms.every((perm: any) => perm.isSelected);
+
+            return {
+              ...dev,
+              permissions: devPerms,
+              is_selected_all
+            };
+          });
+
+          const all_devices_selected = devices.every((d: any) => d.is_selected_all);
+          const all_add_perm = devices.every((d: any) =>
+            d.permissions.find((p: any) => p.name === 'Add')?.isSelected
+          );
+          const all_view_perm = devices.every((d: any) =>
+            d.permissions.find((p: any) => p.name === 'View')?.isSelected
+          );
+          const all_edit_perm = devices.every((d: any) =>
+            d.permissions.find((p: any) => p.name === 'Edit')?.isSelected
+          );
+          const all_delete_perm = devices.every((d: any) =>
+            d.permissions.find((p: any) => p.name === 'Delete')?.isSelected
+          );
+
+          return {
+            deviceType: type.deviceType,
+            devices,
+            all_devices_selected,
+            all_add_perm,
+            all_edit_perm,
+            all_view_perm,
+            all_delete_perm
+          };
+        });
+
+        this.devicesTypes.set(structured);
+
+
+        console.log("devicesTypes" , this.devicesTypes());
+
+        this.loading_data.set(false)
       })
     })
   }
 
-  devicesTypes= signal<any[]>([])
+  devicesTypes = signal<any[]>([])
 
   userDataShow: any;
 
